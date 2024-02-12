aac4e30ae853197e669d68b6f05667d0f22770cd
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-02-12 13:28:42 +0800

    main

--- ch4/js/config/configpeta.js
+++ ch4/js/config/configpeta.js
@@ -1,22 +1,22 @@
-import Map from 'https://cdn.skypack.dev/ol/Map.js';
-import View from 'https://cdn.skypack.dev/ol/View.js';
-import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
-import XYZ from 'https://cdn.skypack.dev/ol/source/XYZ.js';
-import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
-import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
-import {container} from 'https://jscroot.github.io/element/croot.js';
+import Map from "https://cdn.skypack.dev/ol/Map.js";
+import View from "https://cdn.skypack.dev/ol/View.js";
+import TileLayer from "https://cdn.skypack.dev/ol/layer/Tile.js";
+import XYZ from "https://cdn.skypack.dev/ol/source/XYZ.js";
+import OSM from "https://cdn.skypack.dev/ol/source/OSM.js";
+import { fromLonLat } from "https://cdn.skypack.dev/ol/proj.js";
+import Overlay from "https://cdn.skypack.dev/ol/Overlay.js";
+import { container } from "https://jscroot.github.io/element/croot.js";
 
 const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; PetaPedia Indonesia</a> ';
 
-const place = [107.60731332364867, -6.913779059470912];
+const place = [107.44611860881116, -6.555661227604976];
 
-export let idmarker = {id:1};
+export let idmarker = { id: 1 };
 
 const basemap = new TileLayer({
-    source: new OSM({
-        attributions: attributions,
-      }),
+  source: new OSM({
+    attributions: attributions,
+  }),
 });
 
 const defaultstartmap = new View({
@@ -25,29 +25,26 @@ const defaultstartmap = new View({
 });
 
 export const overlay = new Overlay({
-    element: container('popup'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
+  element: container("popup"),
+  autoPan: {
+    animation: {
+      duration: 250,
     },
-  });
+  },
+});
 
 export const popupinfo = new Overlay({
-    element: container('popupinfo'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
+  element: container("popupinfo"),
+  autoPan: {
+    animation: {
+      duration: 250,
     },
+  },
 });
 
 export let map = new Map({
-  layers: [
-        basemap
-    ],
-  overlays: [overlay,popupinfo],
-  target: 'map',
+  layers: [basemap],
+  overlays: [overlay, popupinfo],
+  target: "map",
   view: defaultstartmap,
 });
-
